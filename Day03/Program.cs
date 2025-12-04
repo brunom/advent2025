7c5ed9845a18db2bb68cc6f7840d3c4962d25fb5
bmartinez <bmartinez@inetsat.com>
2025-12-04 20:08:13 -0300

    Day03 part2

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,20 +1,32 @@
-﻿string[] input = {
+﻿static long Joltage(string line, int digits)
+{
+    long result = 0;
+    while (digits > 0)
+    {
+        int maxIndex = line
+            .SkipLast(digits - 1)
+            .Select((ch, i) => (ch, i))
+            .MaxBy(x => x.ch).i;
+        result = result * 10 + (line[maxIndex] - '0');
+        line = line.Substring(maxIndex + 1);
+        digits -= 1;
+    }
+    return result;
+}
+
+string[] input = {
     "987654321111111",
     "811111111111119",
     "234234234234278",
     "818181911112111",
 };
 input = File.ReadAllLines("input.txt");
-int total = 0;
+long total2 = 0;
+long total12 = 0;
 foreach (var line in input)
 {
-    int maxPos = line
-        .SkipLast(1)
-        .Select((ch, i) => (ch, i))
-        .MaxBy(x => x.ch).i;
-    char ch1 = line[maxPos];
-    string rest = line.Substring(maxPos + 1);
-    char ch2 = rest.Max();
-    total += int.Parse(ch1.ToString()+ch2.ToString());
+    total2 += Joltage(line, 2);
+    total12 += Joltage(line, 12);
 }
-Console.WriteLine(total);
+Console.WriteLine(total2);
+Console.WriteLine(total12);
