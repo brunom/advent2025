c06adcd3bb05f73332722a9b8eb6dec545dd3687
bmartinez <bmartinez@inetsat.com>
2025-12-04 11:25:46 -0300

    Day03 part1

---
+++ Day03/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] input = {
+    "987654321111111",
+    "811111111111119",
+    "234234234234278",
+    "818181911112111",
+};
+input = File.ReadAllLines("input.txt");
+int total = 0;
+foreach (var line in input)
+{
+    int maxPos = line
+        .SkipLast(1)
+        .Select((ch, i) => (ch, i))
+        .MaxBy(x => x.ch).i;
+    char ch1 = line[maxPos];
+    string rest = line.Substring(maxPos + 1);
+    char ch2 = rest.Max();
+    total += int.Parse(ch1.ToString()+ch2.ToString());
+}
+Console.WriteLine(total);
