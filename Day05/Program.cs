dc8f868bc5882926bd81aaf92adb3d603c144fcf
bmartinez <bmartinez@inetsat.com>
2025-12-05 21:03:54 -0300

    Day05 part2 open-closed

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -16,14 +16,14 @@ int emptyIndex = Array.IndexOf(lines, "");
 var fresh = lines[0..emptyIndex].Select(s =>
 {
     var parts = s.Split('-').Select(long.Parse).ToArray();
-    return (parts[0], parts[1]);
+    return (parts[0], parts[1] + 1); // open-closed intervals simplify part 2
 
 }).ToArray();
 var available = lines[(emptyIndex + 1)..].Select(long.Parse).ToArray();
 long count1 = 0;
 foreach (var a in available)
 {
-    if (fresh.Any(f => f.Item1 <= a && a <= f.Item2))
+    if (fresh.Any(f => f.Item1 <= a && a < f.Item2))
     {
         count1++;
     }
@@ -32,24 +32,18 @@ Console.WriteLine(count1);
 
 var edges = fresh
     .SelectMany(f => new[] { (ID: f.Item1, d: +1), (ID: f.Item2, d: -1), })
-    .GroupBy(e => e.ID)
-    .Select(g => (ID: g.Key, d: g.Sum(e => e.d)))
     .OrderBy(t => t.ID)
     .ToArray();
 long level = 0;
 long prev = 0;
 long count2 = 0;
-foreach (var e in edges)
+foreach (var (ID, d) in edges)
 {
     if (level > 0)
     {
-        count2 += e.ID - prev;
+        count2 += ID - prev;
     }
-    level += e.d;
-    if (level == 0)
-    {
-        count2++;
-    }
-    prev = e.ID;
+    level += d;
+    prev = ID;
 }
 Console.WriteLine(count2);
