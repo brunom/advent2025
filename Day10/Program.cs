27f0816540fdf741cc5ca64132f477d39cf9fd07
bmartinez <bmartinez@inetsat.com>
2025-12-13 12:27:42 +0000

    Day10 part2 wip

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -6,15 +6,16 @@ string[] lines = {
     "[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}",
 };
 lines = File.ReadAllLines("input.txt");
-int total = 0;
+int total1 = 0;
+int total2 = 0;
 foreach (var line in lines)
 {
     var parts = line.Split(' ');
-    var indicator =
+    var light_goal =
         parts[0].Trim('[', ']')
         .Select((c, i) => (c == '#' ? 1u : 0u) << i)
         .Aggregate(0u, (acc, val) => acc | val);
-    var schematics =
+    var button_effect =
         parts[1..^1]
         .Select(part =>
             part
@@ -22,24 +23,80 @@ foreach (var line in lines)
             .Split(',')
             .Select(int.Parse)
             .Aggregate(0u, (acc, val) => acc | (1u << val)))
+        .OrderByDescending(BitOperations.PopCount)
         .ToArray();
-    int minButtons = int.MaxValue;
-    for (uint i = 0; i < (1 << schematics.Length); i++)
+    var light_joltage_goal =
+        parts[^1]
+        .Trim('{', '}')
+        .Split(',')
+        .Select(int.Parse)
+        .ToArray();
+
+    int minButtons1 = int.MaxValue;
+    for (uint i = 0; i < (1 << button_effect.Length); i++)
     {
         uint tempIndicator = 0;
-        for (int j = 0; j < schematics.Length; j++)
+        for (int j = 0; j < button_effect.Length; j++)
         {
             if (((i >> j) & 1) == 1)
             {
-                tempIndicator ^= schematics[j];
+                tempIndicator ^= button_effect[j];
             }
         }
         int buttons = BitOperations.PopCount(i);
-        if (tempIndicator == indicator && buttons < minButtons)
+        if (tempIndicator == light_goal && buttons < minButtons1)
         {
-            minButtons = buttons;
+            minButtons1 = buttons;
         }
     }
-    total += minButtons;
+    total1 += minButtons1;
+
+    int minButtons2 = int.MaxValue;
+    int[] curr_presses = new int[button_effect.Length];
+    int[] curr_joltage = new int[light_joltage_goal.Length];
+    bool add_joltage(int button, int presses) // returns overflow
+    {
+        bool overflow = false;
+        for (int light = 0; light < curr_joltage.Length; light++)
+        {
+            if (((button_effect[button] >> light) & 1) == 1)
+            {
+                curr_joltage[light] += presses;
+                if (curr_joltage[light] > light_joltage_goal[light])
+                {
+                    overflow = true;
+                }
+            }
+        }
+        return overflow;
+    }
+    bool inc_presses() // returns carry
+    {
+        for (int button = 0; button < button_effect.Length; button++)
+        {
+            curr_presses[button]++;
+            if (!add_joltage(button, 1))
+                return false;
+            add_joltage(button, -curr_presses[button]);
+            curr_presses[button] = 0;
+        }
+        return true;
+    }
+    while (true)
+    {
+        if (inc_presses())
+            break;
+        if (Enumerable.SequenceEqual(curr_joltage, light_joltage_goal))
+        {
+            int buttons = curr_presses.Sum();
+            if (buttons < minButtons2)
+            {
+                minButtons2 = buttons;
+                break;
+            }
+        }
+    }
+    total2 += minButtons2;
+
 }
-Console.WriteLine(new { total });
+Console.WriteLine(new { total1, total2 });
